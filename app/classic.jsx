a52741d0231201a132581d48fb54b2b1390c7e3d
AlexisHzU <alexis.urena21@gmail.com>
2026-08-08 20:06:31 -0400

    fix

---
+++ app/classic.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+
+export default function Classic(){
+    
+    
+    return(
+        <View>
+            <Text>
+                hola viste que cambio la pantalla
+            </Text>
+        </View>
+    )
+}
